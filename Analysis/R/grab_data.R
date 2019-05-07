03014f367df18fb43845b6eb61ef81411f295842
Karl Broman <kbroman@gmail.com>
2019-05-07 14:36:37 -0500

    Fix Analysis/R/grab_data.R: url, temp directories, etc

--- Analysis/R/grab_data.R
+++ Analysis/R/grab_data.R
@@ -10,17 +10,20 @@ clean_zipfile <- "Attie_2015_eqtl_clean.zip"
 tissues <- c("adipose", "gastroc", "hypo", "islet", "kidney", "liver")
 
 # temporary directory
-dir <- tempdir()
-raw_path <- file.path(dir, raw_zipfile)
-clean_path <- file.path(dir, clean_zipfile)
+this_dir <- "."
+temp_dir <- tempdir()
+raw_path <- file.path(this_dir, raw_zipfile)
+clean_path <- file.path(this_dir, clean_zipfile)
 
 # download raw data
 cat(" -Downloading raw zip file (nearly 1 GB)\n")
-utils::download.file(paste0(mpd_url, "/", raw_zipfile), raw_path)
+if(!file.exists(raw_path)) {
+    utils::download.file(paste0(mpd_url, "/", raw_zipfile), raw_path)
+}
 
 # unzip
 cat(" -Unzipping raw data\n")
-unzipped_files <- utils::unzip(raw_path, exdir=dir)
+unzipped_files <- utils::unzip(raw_path, exdir=temp_dir)
 
 # function to read csv file
 myfread <-
@@ -37,7 +40,7 @@ for(tissue in tissues) {
     cat(" ---Reading and saving", tissue, "\n")
     obj <- paste0(tissue, ".mlratio")
     assign(obj,
-           t(myfread(file.path(dir, "Raw", paste0(tissue, "_mlratio_raw.csv")))))
+           t(myfread(file.path(temp_dir, "Raw", paste0(tissue, "_mlratio_raw.csv")))))
     save(list=obj, file=file.path("..", "OrigData", paste0("F2.mlratio.", tissue, ".RData")))
 }
 
@@ -49,10 +52,10 @@ for(tissue in tissues) {
     obj <- paste0(tissue, ".mlratio")
     assign(obj, get(obj)[is.na(match(rownames(get(obj)), omit$MouseNum[omit$Tissue==tissue])),])
 }
-save(list=paste0(tissues, ".mlratio"), file.path("Rcache", "mlratios.RData"))
+save(list=paste0(tissues, ".mlratio"), file=file.path("Rcache", "mlratios.RData"))
 
 # read very raw genotypes and make some slight changes
-suppressWarnings(rawg <- read.cross("csv", file.path(dir, "Raw"), "genotypes_veryraw.csv",
+suppressWarnings(rawg <- read.cross("csv", file.path(temp_dir, "Raw"), "genotypes_veryraw.csv",
                                     genotypes=0:2, convertXdata=FALSE))
 # save plate info
 longid <- as.character(rawg$pheno$longid)
@@ -83,18 +86,20 @@ rawg <- rawg[,nmissing(rawg) < 1000]
 save(rawg, file=file.path("..", "OrigData", "rawg.RData"))
 
 # read raw data; reorder chr numbers
-suppressWarnings(f2g <- read.cross("csv", file.path(dir, "Raw"), "genotypes_raw.csv",
+suppressWarnings(f2g <- read.cross("csv", file.path(temp_dir, "Raw"), "genotypes_raw.csv",
                                    genotypes=c("BB", "BR", "RR"), alleles=c("B", "R")))
 # reorder chromosomes
 f2g$geno <- f2g$geno[c(1:19, "X", "un")]
 save(f2g, file=file.path("..", "OrigData", "clean_cross.RData"))
 
 cat(" -Downloading clean zip file (nearly 1 GB)\n")
-utils::download.file(paste0(mpd_url, "/", clean_zipfile), clean_path)
+if(!file.exists(clean_path)) {
+    utils::download.file(paste0(mpd_url, "/", clean_zipfile), clean_path)
+}
 
 # unzip
 cat(" -Unzipping clean data\n")
-unzipped_files <- utils::unzip(clean_path, exdir=dir)
+unzipped_files <- utils::unzip(clean_path, exdir=temp_dir)
 
 # reorg microarray data
 cat(" -Reading and saving mlratios\n")
@@ -102,23 +107,23 @@ for(tissue in tissues) {
     cat(" ---Reading and saving", tissue, "\n")
     obj <- paste0(tissue, ".mlratio")
     assign(obj,
-           myfread(file.path(dir, "Clean", paste0(tissue, "_mlratio_clean.csv"))))
+           myfread(file.path(temp_dir, "Clean", paste0(tissue, "_mlratio_clean.csv"))))
     save(list=obj, file=file.path("..", "FinalData", paste0(tissue, "_mlratio_final.RData")))
 }
 
 # copy annotation file
-annot <- fread(file.path(dir, "Clean", "microarray_annot.csv"), data.table=FALSE)
+annot <- fread(file.path(temp_dir, "Clean", "microarray_annot.csv"), data.table=FALSE)
 save(annot, file=file.path("..", "OrigData", "annot.amit_rev.RData"))
 
 # clean genotype data
-f2g <- read.cross("csv", file.path(dir, "Clean"), "genotypes_clean.csv",
+f2g <- read.cross("csv", file.path(temp_dir, "Clean"), "genotypes_clean.csv",
                   genotypes=c("BB", "BR", "RR"), alleles=c("B", "R"))
-f2g$geno <- x$geno[c(1:19, "X")]
+f2g$geno <- f2g$geno[c(1:19, "X")]
 
 # genetic and physical maps
-gmap <- myfread(file.path(dir, "Clean", "markers_genetic_map.csv"))
-pmap <- myfread(file.path(dir, "Clean", "markers_physical_map.csv"))
-mnames <- split(rownames(pmap), factor(pmap$chr, levels=c(1:19,"X")))
+gmap <- data.table::fread(file.path(temp_dir, "Clean", "markers_genetic_map.csv"))
+pmap <- data.table::fread(file.path(temp_dir, "Clean", "markers_physical_map.csv"))
+mnames <- split(pmap$marker, factor(pmap$chr, levels=c(1:19,"X")))
 gmap <- split(gmap$pos, factor(gmap$chr, levels=c(1:19,"X")))
 pmap <- split(pmap$pos, factor(pmap$chr, levels=c(1:19,"X")))
 for(i in seq(along=gmap)) {
